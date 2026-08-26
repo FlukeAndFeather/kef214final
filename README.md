db3feff4cc75bd90fa41d7649a08291f9b4380fe
Kate Frankland <katefrankland5@gmail.com>
2026-08-26 12:14:58 -0700

    committing files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Project Overview 
 
-- input information about the articles/data being used, include citations, brief overview of what the figure being generated is 
+- include information about the articles/data being used, include citations, brief overview of what the figure being generated is 
 
 Citations: 
 
@@ -14,16 +14,17 @@ The following datasets are being used for this visualization:
 3. QuebradaCuenca3-Bisley.csv
 4. RioMameyesPuenteRoto.csv
 5. LUQ LTER MDLs.csv
+ 
+ - include brief summary about why these were chosen to be used, where they are located. 
 
 # Filtering Data 
 
- The following datasets were filted to include six columns being the Sample Date,and the ions we are trying to learn more about, including K, Mg, Ca, NO3-N, and NH4-N. The Sample Date column was filtered to select dates ranging from January 1, 1988 to January 1, 1994. 
-
+ The following datasets were filtered to include six columns being the **Sample Date**, and the ions we are trying to learn more about, including **K, Mg, Ca, NO3-N, and NH4-N**. The Sample Date column was filtered to select dates ranging from January 1, 1988 to January 1, 1994. Dates are going to be in a year, month, day format in each file. 
 
 
 1. QuebradaCuenca1-Bisley.csv, cleaned and renamed to Bisley01
-2. QuebradaCuenca2-Bisley.csv cleaned and renamed to Bisley02
-3. QuebradaCuenca3-Bisley.csv cleaned and renamed to Bisley03
+2. QuebradaCuenca2-Bisley.csv, cleaned and renamed to Bisley02
+3. QuebradaCuenca3-Bisley.csv, cleaned and renamed to Bisley03
 
 
 # Finding the Averages  
--- scratch/kef214final_scratch.R
+++ scratch/kef214final_scratch.R
@@ -5,6 +5,8 @@ library(tidyverse)
 Bisley01 <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca1-Bisley.csv")
 Bisley02 <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca2-Bisley.csv")
 Bisley03 <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca3-Bisley.csv")
+PuentoRoto <- read_csv("data/knb-lter-luq.20.4923064/RioMameyesPuenteRoto.csv")
+LTER <- read_csv("data/knb-lter-luq.20.4923064/LUQ LTER MDLs.csv")
 
 
 # Filter Sample_Date so only rows 1988 to 1994 are included 
@@ -13,23 +15,28 @@ Bisley03 <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca3-Bisley.csv")
 
 Bisley01_filtered <- Bisley01 |> 
   select(Sample_Date, K, Mg, Ca, `NO3-N`, `NH4-N`) |> 
-  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1994-01-01")
+  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1994-12-31")
 Bisley01_filtered
 
 Bisley02_filtered <- Bisley02 |> 
   select(Sample_Date, K, Mg, Ca, `NO3-N`, `NH4-N`) |> 
-  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1994-01-01")
+  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1994-12-31")
 Bisley02_filtered
 
 Bisley03_filtered <- Bisley03 |> 
   select(Sample_Date, K, Mg, Ca, `NO3-N`, `NH4-N`) |> 
-  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1994-01-01")
+  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1994-12-31")
 Bisley03_filtered
+
+PuentoRoto_filtered <- PuentoRoto |> 
+  select(Sample_Date, K, Mg, Ca, `NO3-N`, `NH4-N`) |> 
+  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1994-12-31")
+PuentoRoto_filtered
 
 # Now we have three *pretty* datasets <3 
 # Want to combine them all together 
 
-Combined_Bisley_Data <- bind_rows(list(Bisley01_filtered, Bisley02_filtered, Bisley03_filtered))
+Combined_Bisley_Data <- bind_rows(list(Bisley01_filtered, Bisley02_filtered, Bisley03_filtered, PuentoRoto_filtered))
 Combined_Bisley_Data
 
 
@@ -43,7 +50,7 @@ Combined_Bisley_Data
 Bisley_Averages <- tibble(
   window_start = seq(
     ymd("1988-01-05"),
-    ymd("1993-11-16"),
+    ymd("1994-12-27"),
     by = "9 weeks"
   ),
   K = NA,
@@ -62,27 +69,43 @@ for (i in 1:nrow(Bisley_Averages)) {
   
 print(w2)
 
-  in_window <- filter(Bisley_Averages, (window_start >= w1 & window_start < w2))
+  in_window <- filter(Bisley01_filtered, (Sample_Date >= w1 & Sample_Date < w2))
 
 # Find the mean 
 
 Bisley_Averages$K[i] <- mean(in_window$K, na.rm = TRUE)
 Bisley_Averages$Mg[i] <- mean(in_window$Mg, na.rm = TRUE)
-Bisley_Averages$Ca[i] <- mean(in_window$Mg, na.rm = TRUE)
-Bisley_Averages$`N03-N`[i] <- mean(in_window$Mg, na.rm = TRUE)
-Bisley_Averages$`NH4-N`[i] <- mean(in_window$Mg, na.rm = TRUE)
+Bisley_Averages$Ca[i] <- mean(in_window$Ca, na.rm = TRUE)
+Bisley_Averages$`NO3-N`[i] <- mean(in_window$`NO3-N`, na.rm = TRUE)
+Bisley_Averages$`NH4-N`[i] <- mean(in_window$`NH4-N`, na.rm = TRUE)
 }
+
+#  pivot longer ----------------------------------------------------------
+
+Bisley_Averages <- Bisley_Averages |> 
+pivot_longer(cols = c(K, Mg, Ca, `NO3-N`, `NH4-N`), 
+names_to = "Ions", 
+values_to = "Concentrations"
+)
+
+
 
 # ggplot !  --------------------------------------------------------------
 
 # Make line plot with just one file 
 
-Combined_Bisley_Data |> 
-  ggplot(mapping = aes(x = Sample_Date, y = K)) +
-  geom_line() + 
+Bisley_Averages |> 
+  ggplot(mapping = aes(x = window_start, y = Concentrations)) +
+  geom_line() +
+  facet_wrap(~Ions, scales = "free") +
   theme_minimal()
 
 
-# Use pivot and facet wrap 
+
+
+
+# Need to expand this and want to use pivot and facet wrap 
+# We want the years on the x axis 
 # want to use facet_wrap to get a chart of each ion?
   
+
